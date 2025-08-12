fd174ea14d007ebf8339b3ce609b7dadfe343500
Dio Rovelino <63448336+floxydio@users.noreply.github.com>
2025-08-12 15:54:58 +0700

    update compression

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -18,9 +19,20 @@ func main() {
 		vips.Startup(nil)
 		defer vips.Shutdown()
 
+		checkFileMax := 4 * 1024 * 1024
+		var quality int
+		var scale float64
 		file, err := c.FormFile("file")
 		if err != nil {
 			return c.Status(400).JSON(fiber.Map{"error": "Error when upload!"})
+		}
+
+		if file.Size > int64(checkFileMax) {
+			quality = 62
+			scale = 0.25
+		} else {
+			quality = 85
+			scale = 0.50
 		}
 
 		tempPath := "./temp_" + file.Filename
@@ -33,17 +45,19 @@ func main() {
 			return c.Status(500).JSON(fiber.Map{"error": "Failed to load image"})
 		}
 		defer image1.Close()
-		image1.Resize(0.25, vips.KernelLanczos3)
+		image1.Resize(scale, vips.KernelLanczos3)
 
 		ep := vips.NewDefaultJPEGExportParams()
-		ep.Quality = 62
+		ep.Quality = quality
 		ep.StripMetadata = true
 		image1Bytes, _, err := image1.Export(ep)
 		if err != nil {
 			return c.Status(500).JSON(fiber.Map{"error": "Failed to export image"})
 		}
 
-		if err := os.WriteFile("/uploads/output.jpg", image1Bytes, 0644); err != nil {
+		fileNameOutput := fmt.Sprintf("/uploads/output_%s", file.Filename)
+
+		if err := os.WriteFile(fileNameOutput, image1Bytes, 0644); err != nil {
 			return c.Status(500).JSON(fiber.Map{"error": "Failed to save output"})
 		}
 
